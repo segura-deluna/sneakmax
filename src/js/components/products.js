ad2a56adf685452840ce321192126556530f760d
Segura <segura-deluna@mail.ru>
2021-06-21 01:07:00 +0300

    added loaded data-products

---
+++ src/js/components/products.js
@@ -0,0 +1,73 @@
+const catalogList = document.querySelector('.catalog-list');
+const catalogMore = document.querySelector('.catalog__more');
+let prodQuantity = 6;
+let dataLength = null;
+
+const normalPrise = (str) => {
+  return String(str).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+};
+
+if (catalogList) {
+  const loadProducts = (quantity = 6) => {
+    fetch('../data/data.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        dataLength = data.length;
+        catalogList.innerHTML = '';
+        for (let i = 0; i < dataLength; i++) {
+          if (i < quantity) {
+            let item = data[i];
+            catalogList.innerHTML += `
+						<li class="catalog-list__item">
+							<article class="product">
+								<div class="product__image">
+									<img src="${item.mainImage}" alt="${item.title}">
+									<div class="product__btns">
+										<button class="btn-reset product__btn" data-id="${item.id}"
+											data-graph-path="prod-modal" aria-label="Показать информацию о товаре">
+											<svg>
+												<use xlink:href="img/sprite.svg#eye"></use>
+											</svg>
+										</button>
+										<button class="btn-reset product__btn" aria-label="Добавить товар в корзину">
+											<svg>
+												<use xlink:href="img/sprite.svg#cart"></use>
+											</svg>
+										</button>
+									</div>
+								</div>
+								<h3 class="product__title">${item.title}</h3>
+								<span class="product__price">${normalPrise(item.price)} р</span>
+							</article>
+						</li>
+						`;
+          }
+        }
+      })
+      .then(() => {
+        const productTitle = document.querySelectorAll('.product__title');
+        productTitle.forEach((element) => {
+          $clamp(element, { clamp: '40px' });
+        });
+        const modal = new GraphModal({
+          isOpen: (modal) => {
+            const openBtnId = modal.previousActiveElement.dataset.id;
+            console.log(openBtnId);
+          },
+        });
+      });
+  };
+  loadProducts(prodQuantity);
+
+  catalogMore.addEventListener('click', (e) => {
+    prodQuantity = prodQuantity + 3;
+    loadProducts(prodQuantity);
+    if (prodQuantity >= dataLength) {
+      catalogMore.style.display = 'none';
+    } else {
+      catalogMore.style.display = 'block';
+    }
+  });
+}
